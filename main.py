c329fc27fa23b948a1a16401a1f3f86ee6530e7c
Vaishakh P <vaishakhp1998@gmail.com>
2021-10-31 22:09:29 +0530

    cloud engine run

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! CD'
+    return 'Hello World! Bruh'
 
 @app.route('/echo/<name>')
 def echo(name):
